83a0673de41bafd9eeb77eb6e517349b5aec2a2d
Gabriel Otero <gabrielotero@MacBook-Pro-de-Gabriel.local>
2023-05-01 13:13:20 -0300

    Downgrade Android minSdk and version bump

--- app/src/main/java/br/com/ladyplant/ui/components/ResultListShimmer.kt
+++ app/src/main/java/br/com/ladyplant/ui/components/ResultListShimmer.kt
@@ -1,7 +1,13 @@
 package br.com.ladyplant.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
